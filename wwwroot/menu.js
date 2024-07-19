2879ad7ca2e8cf60accf1b53fb5994cc1cc79873
sergey-frv <sergey.frv@yandex.ru>
2024-07-19 19:30:11 +0600

    Add preview push animation, padding

--- wwwroot/menu.js
+++ wwwroot/menu.js
@@ -5,8 +5,11 @@ document.querySelectorAll('.preview').forEach(preview => {
         document.querySelectorAll('.video-container').forEach(v => {
             v.style.display = 'none';
         });
-        video.parentElement.style.display = 'block';
-        document.getElementById("menu").style.display = 'none';
-        video.play();
+        // Добавляем задержку для показа анимации кнопки
+        setTimeout(() => {
+            video.parentElement.style.display = 'block';
+            document.getElementById("menu").style.display = 'none';
+            video.play();
+        }, 700); // Задержка в 700 миллисекунд
     });
 });
